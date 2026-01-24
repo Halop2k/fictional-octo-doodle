11c67b979edb16fc6e5b51bce2ed56cddd059493
Halop2k <smeogorl@gmail.com>
2026-01-24 17:40:10 +0100

    move configs to default module

--- configurations/nixos/nixos/configuration.nix
+++ configurations/nixos/nixos/configuration.nix
@@ -6,49 +6,6 @@
       ./hardware-configuration.nix
     ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  networking.hostName = "nixos"; # Define your hostname.
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/Malta";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "mt_MT.UTF-8";
-    LC_IDENTIFICATION = "mt_MT.UTF-8";
-    LC_MEASUREMENT = "mt_MT.UTF-8";
-    LC_MONETARY = "mt_MT.UTF-8";
-    LC_NAME = "mt_MT.UTF-8";
-    LC_NUMERIC = "mt_MT.UTF-8";
-    LC_PAPER = "mt_MT.UTF-8";
-    LC_TELEPHONE = "mt_MT.UTF-8";
-    LC_TIME = "mt_MT.UTF-8";
-  };
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.halop = {
     isNormalUser = true;
@@ -62,9 +19,5 @@
     ];
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
   system.stateVersion = "25.11";
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,9 +1,58 @@
 # This is your nixos configuration.
 # For home configuration, see /modules/home/*
-{ flake, ... }:
+{ flake, pkgs, ... }:
 {
   imports = [
     flake.inputs.self.nixosModules.common
   ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  networking.hostName = "nixos"; # Define your hostname.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Malta";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "mt_MT.UTF-8";
+    LC_IDENTIFICATION = "mt_MT.UTF-8";
+    LC_MEASUREMENT = "mt_MT.UTF-8";
+    LC_MONETARY = "mt_MT.UTF-8";
+    LC_NAME = "mt_MT.UTF-8";
+    LC_NUMERIC = "mt_MT.UTF-8";
+    LC_PAPER = "mt_MT.UTF-8";
+    LC_TELEPHONE = "mt_MT.UTF-8";
+    LC_TIME = "mt_MT.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
   services.openssh.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
