bc7efea646d5b111a4873f2f8b7b7d747eb15794
Halop2k <smeogorl@gmail.com>
2026-01-25 00:11:52 +0100

    video driver stuff for steam

--- configurations/nixos/nixos/configuration.nix
+++ configurations/nixos/nixos/configuration.nix
@@ -12,5 +12,14 @@
     extraGroups = [ "networkmanager" "wheel" ];
   };
 
+  hardware.graphics = {
+    ## radv: an open-source Vulkan driver from freedesktop
+    enable32Bit = true;
+
+    ## amdvlk: an open-source Vulkan driver from AMD
+    #extraPackages = [ pkgs.amdvlk ];
+    #extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
+  };
+
   system.stateVersion = "25.11";
 }
