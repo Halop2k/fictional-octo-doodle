0f53de23a9388b9aab5d1ee4ec93ae83de2c57eb
Halop2k <smeogorl@gmail.com>
2026-01-26 05:00:44 +0100

    tmux tokyo-night theme

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -13,10 +13,16 @@
     historyLimit = 50000;
     shell = "${pkgs.bash}/bin/bash";
     plugins = with pkgs.tmuxPlugins; [
-       {
-        plugin = power-theme;
+      {
+        plugin = tokyo-night-tmux;
         extraConfig = ''
-           set -g @tmux_power_theme 'gold'
+          set -g @tokyo-night-tmux_theme night
+          set -g @tokyo-night-tmux_transparent 0
+          set -g @tokyo-night-tmux_show_path 1
+          set -g @tokyo-night-tmux_path_format relative
+          set -g @tokyo-night-tmux_show_hostname 1
+          set -g @tokyo-night-tmux_date_format DMY
+          set -g @tokyo-night-tmux_time_format 24H
         '';
       }
       {
@@ -33,7 +39,6 @@
           set -g @contiuum-save-interval '60'
         '';
       }
-      yank
     ];
   };
 }
