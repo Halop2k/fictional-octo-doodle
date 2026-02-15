67a1171911d18636d22869fe75a9fd6fcd2e7dca
Halop2k <smeogorl@gmail.com>
2026-02-15 15:16:55 +0100

    niri and stuff

--- modules/nixos/gui/plasma6.nix
+++ modules/nixos/gui/plasma6.nix
@@ -1,13 +1,25 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
+
   services = {
     displayManager.sddm.enable = true;
     desktopManager.plasma6.enable = true;
+    niri = {
+      enable = true;
+      # config = import ./niri-config.nix { inherit (flake.inputs) self; };
+      settings = {
+        includes = lib.mkAfter [
+          ./config.kdl
+        ];
+      };
+    };
   };
 
   environment.systemPackages = with pkgs; [
     vesktop
     element-desktop
     steam
+    rofi
+    kitty
   ];
 }
